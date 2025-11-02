70eabd5a48529cfd7cc8312fa57ae54a523cc5a5
Erik-codes <eriksoaresz123@gmail.com>
2025-11-02 11:40:34 -0300

    mais desafios

--- modulo_1/Desafio47.py
+++ modulo_1/Desafio47.py
@@ -1,3 +1,4 @@
-for c in range(0, 50+2, 2):
-    print(c)
+for c in range(0, 51):
+    if c % 2 == 0:
+        print(c, end=' ')
 print(f'Esses são todos os números pares de 0 ao número 50. ')
---
+++ modulo_1/Desafio48.py
@@ -0,0 +1,7 @@
+cont = 0
+soma = 0
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        cont += c
+        soma += 1
+print(f'A soma de todos os  {cont} valores solicitados é {soma} ')
