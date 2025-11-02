4e6ff8bdc4724ea51f7585147084a75d01234bc7
Erik-codes <eriksoaresz123@gmail.com>
2025-11-02 12:41:38 -0300

    primeiro commit

--- modulo_1/exercitar_prime.py
+++ modulo_1/exercitar_prime.py
@@ -1,10 +1,3 @@
-nota = float(input('Digite sua nota: '))
-nota2 = float(input('Digite sua segunda nota: '))
-m = (nota + nota2) / 2
-if m < 5:
-    print('Reprovado! ')
-elif 5 <= m < 7:
-    print('Em recuperação! ')
-elif m >= 7:
-    print('Aprovado! ')
-print(f'Sua média é {m:.2f}')
+n = int(input('Digite um número para ver sua tabuada: '))
+for c in range(1, 11):
+    print(f'{n} x {c:2} = \033[30;41m{c*n:2}\033[m ')
